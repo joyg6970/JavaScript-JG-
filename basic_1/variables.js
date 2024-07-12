db57e19dba8ba2da1fb1a84afec446770cf5de1e
joyg6970 <140743177+joyg6970@users.noreply.github.com>
2024-07-12 15:09:41 +0000

    test

---
+++ basic_1/variables.js
@@ -0,0 +1,11 @@
+const accountId = 1223456
+let accountemail= "joy@gmail.com"
+var accountpass= "1223455"
+accuntCity="kolkata"
+
+accountemail = "joytest@gmail.com"
+accountpass = "12244334"
+accuntCity = "south24 pgs"
+
+console.log(accountId);
+console.table([accountId, accountemail, accountpass, accuntCity])
