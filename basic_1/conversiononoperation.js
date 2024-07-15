8bd65e282c9fd483df8d7da52494189d53fe1f72
joyg6970 <140743177+joyg6970@users.noreply.github.com>
2024-07-15 16:08:06 +0000

    conversion of data type

---
+++ basic_1/conversiononoperation.js
@@ -0,0 +1,26 @@
+let score = "33abcc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumbber = Number(score);
+console.log(typeof valueinNumbber);
+
+// "33"=> 33
+//"33abc"=>Nan
+//true=> 1 fasle=> 0
+
+let valueofNum = "joy";
+
+let booleanis = Boolean(valueofNum);
+console.log(booleanis);
+// 1=>  true , 0=> false
+//""=> false, "joy"=> true
+
+let somenum = 22
+
+let stringnum = String(somenum);
+
+console.log(stringnum);
+console.log(typeof stringnum);
+
---
+++ basic_1/datatype1.js
@@ -0,0 +1,18 @@
+"use strict"; // trate all code new version 
+// alert(6 + 6)// we using node js not browser. 
+
+ // code readability should be high. 
+ let name = "joy"
+ let age = 18 
+ let isloggedin = true
+
+ // number => 2 to power 53
+ // bigint 
+ // string =>""
+ // bollean=> true/false
+ // undifiend =>
+    // symol => unique 
+
+ // object
+
+ console.log(typeof null); // object 
